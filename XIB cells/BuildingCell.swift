0c929a70ae0683bb8c9a1f01620457c5af4c9503
yaoshi <yaos5@student.unimelb.edu.au>
2019-03-17 04:46:54 +1100

    ..

--- XIB cells/BuildingCell.swift
+++ XIB cells/BuildingCell.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class BuildingCell: UITableViewCell {
+    
+    @IBOutlet weak var libraryName: UILabel!
+    @IBOutlet weak var walkingMinutes: UILabel!
+    @IBOutlet weak var walkingMeters: UILabel!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
@@ -21,4 +27,8 @@ class BuildingCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    @IBAction func directionButtonPressed(_ sender: UIButton) {
+        let viewController = ViewController()
+//        viewController.plotDirectionsTo(destName: libraryName.text!, lat: ?, long: ?)
+    }
 }
